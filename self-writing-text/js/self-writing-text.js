344b327c7a4b0d2560ebf5188274307ee48d792b
KyleBCox <kylecoxforweb@gmail.com>
2017-06-11 16:11:18 +0100

    Adding initial files

---
+++ self-writing-text/js/self-writing-text.js
@@ -0,0 +1,19 @@
+﻿window.onload = function () {
+    var selfWritingText = document.getElementsByClassName("self-writing-text");
+    for (var i = 0; i <= selfWritingText.length - 1; i++) {
+        var element = selfWritingText[i];
+        var allLetters = element.getElementsByTagName("span");
+        j = showLetter(0, allLetters);
+
+    }
+}
+
+function showLetter(j, allLetters) {
+    if (j < allLetters.length) {
+        setInterval(function () {
+            var letter = allLetters[j];
+            letter.className = "on"
+            showLetter(j++, allLetters)
+        }, 250);
+    }
+}
